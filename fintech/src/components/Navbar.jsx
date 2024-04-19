2699d0723fb0f2db3e44157b3cce1b59241bf8aa
clementgranjou <clement.granjou@epitech.digital>
2024-04-19 15:20:58 +0200

    link to psql db and homepage

--- fintech/src/components/Navbar.jsx
+++ fintech/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import * as React from "react"
 import { Link } from "react-router-dom";
-import { RxHome, RxPerson } from "react-icons/rx";
-import { TbMessageChatbot } from "react-icons/tb";
+import { HiBolt, HiChatBubbleLeftRight, HiHome } from "react-icons/hi2";
 
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <Link className="navItem" to="/Home"><RxHome/>Home</Link>
-      <Link className="navItem" to="/Chatbot"><TbMessageChatbot/>Chatbot</Link>
-      <Link className="navItem" to="/Account"><RxPerson/>Account</Link>
+      <Link className="navItem" to="/Home"><HiHome className="navbaricon"/>
+</Link>
+      <Link className="navItem" to="/Chatbot"><HiChatBubbleLeftRight className="navbaricon"/></Link>
+      <Link className="navItem" to="/Account"><HiBolt className="navbaricon"/></Link>
     </nav>
   );
 }
--- fintech/src/pages/HomePage.jsx
+++ fintech/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
+import React, { useEffect, useState } from "react";
+
+// shadcn components
 import {
   Table,
   TableBody,
@@ -8,26 +9,95 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
+// icons
+import { HiMiniAdjustmentsHorizontal, HiMiniCursorArrowRays } from "react-icons/hi2";
 
 export default function Home() {
+  const [transactions, setTransactions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/transactions")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setTransactions(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  function formatDate(dateStr) {
+    const date = new Date(dateStr);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  }
+
   return (
-    <div>
-      <h1 className="title">Hi Firstname 👋</h1>
-      <Button>Salut !</Button>
+    <div className="container grid gap-8">
+      <div className="header">
+        <h1 className="title text-slate-800">Hi Firstname 👋</h1>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="header">
+        <h3 className="text-slate-800">Solde de tout compte</h3>
+        <h3 className="text-slate-400">1 compte connecté</h3>
+      </div>
+      <h1 className="text-slate-800 text-3xl font-semibold">$3000.89</h1>
+
+      <div className="grid gap-2">
+        <div className="header">
+          <h3 className="text-slate-800">Suivi des opérations</h3>
+          <HiMiniAdjustmentsHorizontal />
+        </div>
+        <Button className="w-full bg-violet-700 text-slate-50 font-normal gap-x-8">Conseiller moi<HiMiniCursorArrowRays/></Button>
+      </div>
+
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead>Amount</TableHead>
+            <TableHead>Date</TableHead>
             <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
+            <TableHead>Description</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-          </TableRow>
+          {transactions.map((transaction) => (
+            <TableRow key={transaction.transactionid}>
+              <TableCell className="font-bold">
+                {transaction.transactionid}
+              </TableCell>
+              <TableCell>{transaction.amount} USD</TableCell>
+              <TableCell>{formatDate(transaction.transactiondate)}</TableCell>
+              <TableCell>
+                <Badge variant="outline">{transaction.status}</Badge>
+              </TableCell>
+              <TableCell>{transaction.description}</TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </div>
